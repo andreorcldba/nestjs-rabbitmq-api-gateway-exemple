d7a5bc5428ca7ee9e8f489bd5eb63522327a56e7
André Vargas <andre.orcl.dba@gmail.com>
2023-03-18 22:21:13 -0300

    fix: exceptions handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { ResponseInterceptor } from './interceptors/response.interceptor';
 import { AuthenticationsModule } from './modules/authentications/authentications.module';
 import { HelperModule } from './modules/helper/helper.module';
 
 @Module({
   imports: [UsersModule, AuthenticationsModule, HelperModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: ResponseInterceptor
-    }
-  ]
+  providers: [AppService]
 })
 export class AppModule {}
